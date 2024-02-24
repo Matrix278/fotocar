e20b7ce484cb8a3c1fc0bfce9ec6165c4cca1785
Martin Sidorov <martin.sidorov27@gmail.com>
2024-02-24 14:45:45 +0200

    Connect js script to change copyright year

--- js/main.js
+++ js/main.js
@@ -1,22 +1,6 @@
-// Burger menus
-document.addEventListener('DOMContentLoaded', function() {
-    // open/close
-    const toggler = document.querySelectorAll('[data-toggle="side-menu"]');
-
-    if (toggler.length) {
-        for (var i = 0; i < toggler.length; i++) {
-            const target = toggler[i].getAttribute('data-target');
-
-            if (target.length) {
-                toggler[i].addEventListener('click', function(event) {
-                    event.preventDefault();
-                    const menu = document.querySelector(target);
-        
-                    if (menu) {
-                        menu.classList.toggle('d-none');
-                    }
-                });
-            }
-        }
-    }
-});
+//------------------------Change copyright year------------------------//
+var date = new Date();
+if (date.getFullYear() > 2024) {
+  document.querySelector("#copyright-year").innerText =
+    " - " + date.getFullYear();
+}
